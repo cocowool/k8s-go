616a0d92abfb63f91b8efed08254c2e33073fe09
Green Wang <cocowool@qq.com>
2018-07-24 09:28:16 +0800

    add getNodeImages.sh

---
+++ getNodeImages.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker pull registry.cn-hangzhou.aliyuncs.com/k8sth/kube-proxy-amd64:v1.11.0
+docker pull registry.cn-hangzhou.aliyuncs.com/k8sth/pause-amd64:3.1
+docker tag registry.cn-hangzhou.aliyuncs.com/k8sth/pause-amd64:3.1 k8s.gcr.io/pause-amd64:3.1
+docker tag registry.cn-hangzhou.aliyuncs.com/k8sth/kube-proxy-amd64:v1.11.0 k8s.gcr.io/kube-proxy-amd64:v1.11.0
